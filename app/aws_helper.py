521a7b46089b8d3c20d2cd8df4cb7d419a23fb94
家雯 徐 <a0925077973@gmail.com>
2025-06-22 23:36:25 +0800

    14

--- app/aws_helper.py
+++ app/aws_helper.py
@@ -1,12 +1,20 @@
 import json
 from aiobotocore.session import get_session
+from aiohttp import TCPConnector
 
 INPUT_BUCKET = "nicoproject2input"
 REQUEST_QUEUE_URL = "https://sqs.ap-northeast-2.amazonaws.com/530751794867/project2-request-q"
 
+_session = get_session()
+_connector = TCPConnector(limit=128)
+
 async def upload_img_to_s3(file_bytes: bytes, key: str):
-    session = get_session()
-    async with session.create_client('s3', region_name='ap-northeast-2') as s3:
+    """非同步上傳圖片到 S3 input bucket"""
+    async with _session.create_client(
+        's3',
+        region_name='ap-northeast-2',
+        connector=_connector
+    ) as s3:
         await s3.put_object(
             Bucket=INPUT_BUCKET,
             Key=key,
@@ -14,10 +22,16 @@ async def upload_img_to_s3(file_bytes: bytes, key: str):
         )
 
 async def send_request_to_q(request_id: str, s3_key: str):
-    """非同步傳送訊息到 SQS"""
-    session = get_session()
-    async with session.create_client('sqs', region_name='ap-northeast-2') as client:
-        message = json.dumps({"requestId": request_id, "s3Key": s3_key})
+    """非同步送訊息到 SQS request queue"""
+    async with _session.create_client(
+        'sqs',
+        region_name='ap-northeast-2',
+        connector=_connector
+    ) as client:
+        message = json.dumps({
+            "requestId": request_id,
+            "s3Key": s3_key
+        })
         await client.send_message(
             QueueUrl=REQUEST_QUEUE_URL,
             MessageBody=message
