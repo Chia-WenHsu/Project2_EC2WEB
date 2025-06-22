2587210a579ab89a0aee99a9a7afcbf1a249408a
家雯 徐 <a0925077973@gmail.com>
2025-06-22 17:51:43 +0800

    改非同步

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from app.AutoScaler import scale_app_instances
 from app.AutoScaler import get_sqs_q_depth
 from app.AutoScaler import get_current_app_instance
 import asyncio
+import os
 
 app = FastAPI()  
 
@@ -13,18 +14,20 @@ app.include_router(test_router)
 
 @app.on_event("startup")
 async def startup_event():
-    await asyncio.sleep(5) 
+    await asyncio.sleep(5)
     print("System ready. Web API starting...")
 
-    asyncio.create_task(start_autoscaler_loop())
-
+    if os.getpid() == int(os.environ.get("MAIN_GUNICORN_PID", "0")):
+        print("Launching AutoScaler (main process only)")
+        asyncio.create_task(start_autoscaler_loop())  # 
+#  自動 scaling 的背景 loop
 async def start_autoscaler_loop():
     while True:
         try:
-            scale_app_instances()
+            await scale_app_instances()  # 
         except Exception as e:
             print(f"[AutoScaler Error] {e}")
-        await asyncio.sleep(5)  
+        await asyncio.sleep(5) 
 
 
 @app.get("/status")
